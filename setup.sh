d904bc5396febcd1760f7eb374faa42a56e32792
Connor Ruggles <cruggles@iastate.edu>
2017-11-12 15:07:13 -0600

    move cava to config folder

--- setup.sh
+++ setup.sh
@@ -119,12 +119,13 @@ install_configs() {
 
 	ln -fs "$(readlink -f .vim)" "$CONFDIR/.vim"
 	ln -fs "$(readlink -f cava)" "$CONFDIR/.config/cava"
+	ln -fs "$(readlink -f config/i3)" "$CONFDIR/.config/i3"
 	ln -fs "$(readlink -f .bash_aliases)" "$CONFDIR/.bash_aliases"
 	ln -fs "$(readlink -f .bashrc)" "$CONFDIR/.bashrc"
 	ln -fs "$(readlink -f .profile)" "$CONFDIR/.profile"
 	ln -fs "$(readlink -f .tmux.conf)" "$CONFDIR/.tmux.conf"
 	ln -fs "$(readlink -f .xinitrc)" "$CONFDIR/.xinitrc"
-	ln -fs "$(readlink -f .muttrc)" "$CONFDIR/.muttrc"
+	cp -f "$(readlink -f .muttrc)" "$CONFDIR/.muttrc"
 
 	vim +PlugClean +qall
 	vim +PlugInstall +qall
